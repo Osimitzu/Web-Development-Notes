cfa529df8341e138caeb495192194edb1451fe01
Osimitzu <angel_aocc@hotmail.com>
2024-01-01 22:48:30 -0600

    little changes without commit before holidays

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,15 +44,15 @@ const config = {
           sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          // editUrl:
+          //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          // editUrl:
+          //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
           customCss: "./src/css/custom.css",
@@ -76,15 +76,21 @@ const config = {
           {
             type: "docSidebar",
             sidebarId: "tutorialSidebar",
-            position: "left",
-            label: "Tutorial",
-          },
-          { to: "/blog", label: "Blog", position: "left" },
-          {
-            href: "https://github.com/facebook/docusaurus",
-            label: "GitHub",
             position: "right",
+            label: "Notes",
           },
+          // {
+          //   type: "docSidebar",
+          //   sidebarId: "frontendSidebar",
+          //   position: "right",
+          //   label: "Frontend",
+          // },
+          { to: "/blog", label: "Blog", position: "right" },
+          // {
+          //   href: "https://github.com/Osimitzu/Web-Development-Notes",
+          //   label: "GitHub",
+          //   position: "right",
+          // },
         ],
       },
       footer: {
